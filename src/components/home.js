88207e036e0023c3c06c276e517b8245c435c860
qzmp10 <loganrobitaille@outlook.com>
2022-11-08 11:26:28 -0500

    styled components

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import {useState, useEffect} from 'react'
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import { Button }  from './styled-comp'
+import { styled } from 'styled-components'
+
+export default function Home() {
+    return(
+        <Button> 
+            Text
+        </Button>
+    )
+}
---
+++ src/components/styled-comp.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+width: 12rem;
+height: 3.5rem;
+background-color: rgb(255, 57, 57);
+border: none;
+border-radius: .6rem;
+color: white;
+`
