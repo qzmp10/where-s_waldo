c5b5959d5a34859056f4af9d99648e98529b79c6
qzmp10 <loganrobitaille@outlook.com>
2022-11-12 20:46:49 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, useInsertionEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import {
   collection, getDocs, doc, setDoc, Timestamp, updateDoc, serverTimestamp,
@@ -11,6 +11,7 @@ import './App.css';
 import Home from './components/home';
 import Nav from './components/nav';
 import Level1 from './components/Level1';
+import Level2 from './components/Level2';
 import Leaderboard from './components/leaderboard';
 
 
@@ -19,20 +20,22 @@ function App() {
   const [currentLevel, setCurrentLevel] = useState(0);
   const [currentCharacters, setCurrentCharacters] = useState(['Default', 'Default', 'Default']);
 
-  // const nextLevel = () => {
-  //   changeLevel();
-  // }
 
   useEffect(() => {
+    console.log(currentLevel)
     whichLevel();
   }, [])
 
   async function getCharacters() {
-    const charRef = await doc(db, 'waldoData', `level${currentLevel}`);
-    const charSnap = await getDoc(charRef);
-    console.log(charSnap.data()['characters']);
-    const array = charSnap.data()['characters'];
-    setCurrentCharacters(array);
+    if (currentLevel !== 0) {
+      const charRef = await doc(db, 'waldoData', `level${currentLevel}`);
+      const charSnap = await getDoc(charRef);
+      console.log(charSnap.data());
+      console.log(charSnap.data()['characters']);
+      const array = charSnap.data()['characters'];
+      setCurrentCharacters(array);
+    } 
+
   }
 
   async function setLevel(state0, state1, state2, state3) {
@@ -53,10 +56,8 @@ function App() {
     const levelSnap = await getDoc(levelRef);
 
     if (levelSnap.exists()) {
-      console.log(levelSnap.data());
       for (const level in levelSnap.data()) {
         if (levelSnap.data()[level] === true) {
-          console.log(levelSnap.data());
           setCurrentLevel(level);
         }
       }
@@ -68,8 +69,10 @@ function App() {
       <Nav />
       <Routes>
         <Route path='/' element={<Home setLevel={setLevel} />}></Route>
-        <Route path='/level1' element={<Level1 currentLevel={currentLevel} characters={currentCharacters} setLevel={setLevel} getCharacters={getCharacters}/>}></Route>
-        <Route path='/leaderboard' element={<Leaderboard/>}></Route>
+        <Route path='/level1' element={<Level1 currentLevel={currentLevel} characters={currentCharacters}
+          setLevel={setLevel} getCharacters={getCharacters} whichLevel={whichLevel} />}></Route>
+        <Route path='/level2' element={<Level2 currentLevel={currentLevel} characters={currentCharacters} setLevel={setLevel} getCharacters={getCharacters} />}></Route>
+        <Route path='/leaderboard' element={<Leaderboard />}></Route>
       </Routes>
     </Router>
   );
--- src/components/Level2.js
+++ src/components/Level2.js
@@ -1,36 +1,29 @@
-import { useState, useEffect, useRef } from 'react'
-import { Link } from 'react-router-dom'
-import waldo1 from '../waldo1.jpg'
-import { Button, Character } from './styled-comp'
-import {doc, updateDoc, getDoc,
+import { useState, useRef, useEffect } from 'react'
+import { Character, Button } from './styled-comp';
+import { Link } from 'react-router-dom';
+import {
+    doc, updateDoc, getDoc,
 } from "firebase/firestore";
-import { db } from '../firebase.config'
-import Pop from './popUp'
-import TimePop from './finishLevel'
+import { db } from '../firebase.config';
+import level2 from '../level2.jpg'
+import Pop from './popUp';
+import TimePop from './finishLevel';
 
-export default function Level1(props) {
-
-
+export default function Level2(props) {
     const [popUpState, setPopUpState] = useState(false);
     const [coordinates, setCoordinates] = useState([]);
     const [positionArray, setPositionArray] = useState([]);
     const [clickPos, setClickPos] = useState('');
     const [levelFinished, setLevelFinished] = useState(false);
 
-    const odlawRef = useRef();
-    const waldoRef = useRef();
-    const wizardRef = useRef();
-
-    const odlawImg = useRef();
     const waldoImg = useRef();
-    const wizardImg = useRef();
+    const waldoRef = useRef();
 
     const userTime = useRef(0);
     const count = useRef(0);
 
-
     useEffect(() => {
-        props.setLevel(false, true, false, false);
+        props.setLevel(false, false, true, false);
         props.getCharacters();
 
         getAllPositions();
@@ -42,6 +35,7 @@ export default function Level1(props) {
                 setLevelFinished(true);
             }
             userTime.current = userTime.current + 1;
+            console.log(userTime.current);
         }, 1000);
 
         return () => {
@@ -49,35 +43,25 @@ export default function Level1(props) {
         }
     }, [])
 
-    useEffect(() => {
-
-    }, [])
-
-
     function sendDataToFirebase() {
         if (count === 0) {
-            const odlawPos = odlawRef.current.offsetLeft
             const waldoPos = waldoRef.current.offsetLeft
-            const wizardPos = wizardRef.current.offsetLeft
-            setPosOdlaw(odlawPos);
             setPosWaldo(waldoPos);
-            setPosWizard(wizardPos);
             console.log('data sent to firebase')
         } else {
             return;
         }
     }
 
+
     const clicky = (e) => {
-        if (positionArray.includes(e.target.offsetLeft) && e.target.offsetLeft === positionArray[0]) {
-            setClickPos(e.target.offsetLeft);
-        } else if (positionArray.includes(e.target.offsetLeft) && e.target.offsetLeft === positionArray[1]) {
-            setClickPos(e.target.offsetLeft);
-        } else if (positionArray.includes(e.target.offsetLeft) && e.target.offsetLeft === positionArray[2]) {
-            setClickPos(e.target.offsetLeft);
-        } else {
-            setClickPos('');
-        }
+        console.log(e.target.offsetLeft);
+        // if (positionArray.includes(e.target.offsetLeft) && e.target.offsetLeft === positionArray[0]) {
+
+        //     setClickPos(e.target.offsetLeft);
+        // } else {
+        //     setClickPos('');
+        // }
         setCoordinates([e.pageX, e.pageY]);
         setPopUpState(true);
 
@@ -85,16 +69,8 @@ export default function Level1(props) {
 
     const callback = (popState, selection) => {
         setPopUpState(popState);
-        if (selection === 'odlaw') {
-            odlawImg.current.style.opacity = '0.3';
-            count.current = count.current + 1;
-        }
         if (selection === 'waldo') {
             waldoImg.current.style.opacity = '0.3';
-            count.current = count.current + 1;
-        }
-        if (selection === 'wizard') {
-            wizardImg.current.style.opacity = '0.3';
             count.current = count.current + 1;
         } else {
             return;
@@ -105,37 +81,22 @@ export default function Level1(props) {
         setLevelFinished(false);
     }
 
-    async function setPosOdlaw(position) {
-        const ref = doc(db, 'waldoData', 'level1');
-
-        await updateDoc(ref, {
-            odlaw: position,
-        })
-    }
 
     async function setPosWaldo(position) {
-        const ref = doc(db, 'waldoData', 'level1');
+        const ref = doc(db, 'waldoData', 'level2');
 
         await updateDoc(ref, {
             waldo: position,
         })
     }
 
-    async function setPosWizard(position) {
-        const ref = doc(db, 'waldoData', 'level1');
-
-        await updateDoc(ref, {
-            wizard: position,
-        })
-    }
-
     async function getAllPositions() {
-        const ref = doc(db, 'waldoData', 'level1');
+        const ref = doc(db, 'waldoData', 'level2');
         const refSnap = await getDoc(ref);
-        const array = [refSnap.data()['odlaw'], refSnap.data()['waldo'], refSnap.data()['wizard']]
-        console.log(array)
+        const array = [refSnap.data()['waldo']]
         setPositionArray(array);
     }
+
 
 
     return (
@@ -144,19 +105,15 @@ export default function Level1(props) {
                 <div className='levelContainer'>
                     <div className='levelHeader'>
                         <div className='levelAndCharacters'>
-                            <div className='currentLevel'>Waldo's Beach</div>
+                            <div className='currentLevel'>Waldo's Plaza</div>
                             <div className='headerCharacters'>
-                                <Character ref={odlawImg} className='odlaw' alt='odlaw'
-                                    src='https://www.giantbomb.com/a/uploads/scale_small/4/46311/1333591-200px_character.odlaw.jpg' />
                                 <Character ref={waldoImg} className='waldo' alt='odlaw'
                                     src='https://www.giantbomb.com/a/uploads/scale_small/0/5973/545186-waldo2.jpg' />
-                                <Character ref={wizardImg} className='wizard' alt='odlaw'
-                                    src='https://www.giantbomb.com/a/uploads/scale_small/4/46311/1341868-wizard.gif' />
                             </div>
                         </div>
                         <Link to='/level2'>
                             <Button>
-                               Next Level >
+                           Next Level >
                             </Button>
                         </Link>
 
@@ -164,10 +121,8 @@ export default function Level1(props) {
                     <div className='waldoMap' onClick={clicky} onMouseOver={() => {
                         sendDataToFirebase();
                     }}>
-                        <img src={waldo1} alt='waldo-wallpaper' />
-                        <div ref={odlawRef} className='hitSquare1'></div>
-                        <div ref={waldoRef} className='hitSquare2'></div>
-                        <div ref={wizardRef} className='hitSquare3'></div>
+                        <img src={level2} alt='waldo-wallpaper' />
+                        <div ref={waldoRef} className='hitSquare1-lvl2'></div>
                     </div>
                 </div>
                 {levelFinished === true ? (
